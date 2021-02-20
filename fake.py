ab426749d5b84292c9aff0d832a85e44fd048f66
Glady-458 <suvajit.sj@gmail.com>
2021-02-20 09:24:02 +0000

    Passive Aggressive Classifier

--- fake.py
+++ fake.py
@@ -3,6 +3,7 @@ import pandas as pd
 import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
 
 df = pd.read_csv('E:\\Explore\\Fake News detector\\news\\news.csv')
 print(df.head())
@@ -13,5 +14,8 @@ x_train, x_test, y_train, ytest = train_test_split(df['text'], labels, test_size
 
 tfidf_vect = TfidfVectorizer(stop_words="english", max_df=0.7)
 
-tfid_train = tfidf_vect.fit_transform(x_train)
+tfidf_train = tfidf_vect.fit_transform(x_train)
 tfidf_test = tfidf_vect.transform(x_test)
+
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train, y_train)
