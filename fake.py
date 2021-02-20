cafa6e2c286f854f5b1de9a53352225d7e788f29
Glady-458 <suvajit.sj@gmail.com>
2021-02-20 08:48:28 +0000

    Reading data and spliting

--- fake.py
+++ fake.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
-import intertools
-from sklearn.model_Selection import train_test_split
+import itertools
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+df = pd.read_csv('E:\\Explore\\Fake News detector\\news\\news.csv')
+print(df.head())
+labels = df.label
+print(labels.head())
+
+x_train, xtest, y_train, ytest = train_test_split(df['text'], labels, test_size = 0.2, random_state=2)
